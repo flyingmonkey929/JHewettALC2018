b5bd0579f2ed8c28eb9c6b6f89278ef540744087
Unknown <flyingmonkey929@gmail.com>
2017-10-11 13:07:53 -0600

    Made Player Move

---
+++ Ghost House/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+		public float movespeed;
+		public float turnspeed;
+		public float jumpheight;
+
+	
+		
+
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		var j = Input.GetAxis("Jump")* Time.deltaTime * jumpheight;
+		var y = Input.GetAxis("Horizontal")* Time.deltaTime * turnspeed;
+		var z = Input.GetAxis("Vertical")* Time.deltaTime * movespeed;
+
+		transform.Rotate(0,y,0);
+		transform.Translate(0,0,z);
+		transform.Translate(0,j,0);
+		
+
+	
+	}
+}
