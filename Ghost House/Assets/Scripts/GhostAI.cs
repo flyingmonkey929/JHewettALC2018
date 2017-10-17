a95a0266a6bc49ef18ae2cb9cbd0218df8bb8cba
Unknown <flyingmonkey929@gmail.com>
2017-10-17 13:01:54 -0600

    Fixed Code added FlashLight

---
+++ Ghost House/Assets/Scripts/GhostAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostAI : MonoBehaviour 
+{
+
+	public float moveSpeed;
+	
+	public Transform target;
+	
+	public int damage;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player")
+		{
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}	
+}
