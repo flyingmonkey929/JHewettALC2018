abe94e33aaa03f874e6d63b2e9dc7d2c066e06f4
Unknown <flyingmonkey929@gmail.com>
2017-10-25 13:10:09 -0600

    Created Wall and FlashLight Script

---
+++ Ghost House/Assets/Scripts/FlashLight.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FlashLight : MonoBehaviour {
+
+	public bool LightOn;
+
+	Light light;
+
+	// Use this for initialization
+	void Start () 
+	{
+		light = GetComponent<Light> ();
+
+		lightOn = true;
+		light.enabled = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	if(Input.GetKeyUp (Key.Code.L) && lightOn){
+		lightOn = false;
+		light.enabled = false;
+
+	}
+	else if (Input.GetKeyUp (KeyCode.L) && !lightOn){
+		lightOn = true;
+		light.enabled = true;
+	}
+		
+		}
+	}
+}
