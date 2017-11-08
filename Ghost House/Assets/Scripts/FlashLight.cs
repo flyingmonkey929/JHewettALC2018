24a8e7ca71df54b347ed96d179ee372ffbc74d8f
Unknown <flyingmonkey929@gmail.com>
2017-11-08 13:12:45 -0700

    added ghost Stun nextime fix errors in flashlight.cs

--- Ghost House/Assets/Scripts/FlashLight.cs
+++ Ghost House/Assets/Scripts/FlashLight.cs
@@ -4,13 +4,12 @@ using System.Collections.Generic;
 
 public class FlashLight : MonoBehaviour {
 
-	public bool LightOn;
+	public bool lightOn;
 
 	Light light;
 
 	// Use this for initialization
-	void Start () 
-	{
+	void Start () {
 		light = GetComponent<Light> ();
 
 		lightOn = true;
@@ -19,13 +18,13 @@ public class FlashLight : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
-	if(Input.GetKeyUp (Key.Code.L) && lightOn){
+	void Update () {
+	if (Input.GetKeyUp (KeyCode.L) && lightOn){
 		lightOn = false;
 		light.enabled = false;
 
 	}
+
 	else if (Input.GetKeyUp (KeyCode.L) && !lightOn){
 		lightOn = true;
 		light.enabled = true;
@@ -33,4 +32,10 @@ public class FlashLight : MonoBehaviour {
 		
 		}
 	}
-}
+	public void setLightOn(){
+		lightOn = true;
+
+	}
+	public bool isLightOn(){
+		return lightOn;
+	}
---
+++ Ghost House/Assets/Scripts/GhostStun.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostStun : MonoBehaviour {
+
+	bool lightCheck;
+	FlashLight flash;
+	public GameObject ghost;
+
+	// Use this for initialization
+	void Start () {
+
+		flash = gameObject.GetComponentInChildren<Light>().GetComponentInChildren<FlashLight>();
+
+		print("Obj:"+flash);
+		flash.setLightOn();
+		print("Start" + flash.isLightOn());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+		void OnTriggerEnter(Collider other){
+			print(other.gameObject.name);
+			print ("Collider" + flash);
+			
+				if(other.gameObject.name == "Ghost" && flash == true ){
+				print("Ghost is stunned");
+
+				other.GetComponent<GhostAI>().moveSpeed = 0f;
+				StartCoroutine(Wait(5));
+			}
+		}
+
+		IEnumerator Wait(float time){
+			yield return new WaitForSeconds(time);
+			ghost.GetComponent<GhostAI>().moveSpeed = 5f;
+			print("Ghost is unstunned");
+		}
+	
+	
+}
