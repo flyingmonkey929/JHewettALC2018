388400cb44b5a538017a93fbdc49cee4325ae178
Unknown <flyingmonkey929@gmail.com>
2017-11-27 12:42:12 -0700

    Added Battery and Battery Scripts

---
+++ Ghost House/Assets/Scripts/BatteryScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatteryScript : MonoBehaviour {
+
+
+	public int power = 4;
+
+	public GameObject flashlight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Player"){
+			flashlight.gameObject.GetComponentInChildren<FlashLight>().currentPower = power;
+			Destroy(gameObject);
+		}
+
+
+	}
+
+
+}
--- Ghost House/Assets/Scripts/FlashLight.cs
+++ Ghost House/Assets/Scripts/FlashLight.cs
@@ -5,11 +5,19 @@ using System.Collections.Generic;
 public class FlashLight : MonoBehaviour {
 
 	public bool lightOn;
+	// Flashlight power capacity
+	public int maxPower = 4;
+	// Usable flashlight power
+	public int currentPower;
 
 	Light light;
 
 	// Use this for initialization
 	void Start () {
+		//Add power to flashlight
+		currentPower = maxPower;
+		print("Power = " + currentPower);
+
 		light = GetComponent<Light> ();
 
 		lightOn = true;
