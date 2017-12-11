98c4d820726bcc36d60c28536159d125387e2874
Unknown <flyingmonkey929@gmail.com>
2017-12-11 12:38:25 -0700

    Made a Garbage smooth camera

---
+++ Ghost House/Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera : MonoBehaviour {
+	public Transform target;
+	public Vector3 defaultDistance = new Vector3(0f,2f,-10f);
+	public float distanceDamp = 10f;
+	public float rotationalDamp = 10f;
+
+
+
+
+
+	Transform myT;
+
+	void Awake()
+	{
+		myT = transform;
+
+	}
+	void LateUpdate()
+	{
+		Vector3 toPos = target.position = (target.rotation * defaultDistance);
+		Vector3 curPos = Vector3.Lerp(myT.position, toPos, distanceDamp * Time.deltaTime);
+		myT.position = curPos;
+
+		Quaternion toRot = Quaternion.LookRotation(target.position - myT.position, target.up);
+		Quaternion curRot = Quaternion.Slerp(myT.rotation, toRot, rotationalDamp * Time.deltaTime);
+		myT.rotation = curRot;
+		
+	}
+	
+}
